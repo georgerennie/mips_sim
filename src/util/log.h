f9e3b06655bf7f1dd7831e69e6ead2b9a6791e20
George Rennie <georgerennie@gmail.com>
2022-04-06 14:30:53 +0100

    Run clang-format

--- src/util/log.h
+++ src/util/log.h
@@ -5,9 +5,9 @@
 extern "C" {
 #endif
 
+#include <inttypes.h>
 #include "core/arch_state.h"
 #include "util.h"
-#include <inttypes.h>
 
 #if defined(ESBMC)
 	#include <assert.h>
@@ -48,23 +48,22 @@ extern "C" {
 #define log_assert_fail(...) log_assert_fmt(0, __VA_ARGS__)
 
 #define log_assert_cond(A, COND, B, FMT) \
-	log_assert_fmt((A) COND (B), #A " = " FMT ", " #B " = " FMT "\n", A, B)
-#define log_assert_condi(A, COND, B) \
-	log_assert_cond(A, COND, B, "%" PRIu32)
+	log_assert_fmt((A) COND(B), #A " = " FMT ", " #B " = " FMT "\n", A, B)
+#define log_assert_condi(A, COND, B) log_assert_cond(A, COND, B, "%" PRIu32)
 
-#define log_assert_eq(A, B, FMT) log_assert_cond(A, ==, B, FMT)
+#define log_assert_eq(A, B, FMT)  log_assert_cond(A, ==, B, FMT)
 #define log_assert_neq(A, B, FMT) log_assert_cond(A, !=, B, FMT)
-#define log_assert_gt(A, B, FMT) log_assert_cond(A, >, B, FMT)
+#define log_assert_gt(A, B, FMT)  log_assert_cond(A, >, B, FMT)
 #define log_assert_gte(A, B, FMT) log_assert_cond(A, >=, B, FMT)
-#define log_assert_lt(A, B, FMT) log_assert_cond(A, <, B, FMT)
+#define log_assert_lt(A, B, FMT)  log_assert_cond(A, <, B, FMT)
 #define log_assert_lte(A, B, FMT) log_assert_cond(A, <=, B, FMT)
 
 // Variants with format specifiers for u32s already
-#define log_assert_eqi(A, B) log_assert_condi(A, ==, B)
+#define log_assert_eqi(A, B)  log_assert_condi(A, ==, B)
 #define log_assert_neqi(A, B) log_assert_condi(A, !=, B)
-#define log_assert_gti(A, B) log_assert_condi(A, >, B)
+#define log_assert_gti(A, B)  log_assert_condi(A, >, B)
 #define log_assert_gtei(A, B) log_assert_condi(A, >=, B)
-#define log_assert_lti(A, B) log_assert_condi(A, <, B)
+#define log_assert_lti(A, B)  log_assert_condi(A, <, B)
 #define log_assert_ltei(A, B) log_assert_condi(A, <=, B)
 
 // Log printf style format
